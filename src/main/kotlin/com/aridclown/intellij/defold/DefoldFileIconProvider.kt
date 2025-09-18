3fc0d16c6ade1c77710053693399141611a9e7fd
Diogo <diogosouzac@gmail.com>
2025-09-18 16:07:16 -0400

    feat: enhance Defold file icon provider with comprehensive file extension mapping

--- src/main/kotlin/com/aridclown/intellij/defold/DefoldFileIconProvider.kt
+++ src/main/kotlin/com/aridclown/intellij/defold/DefoldFileIconProvider.kt
@@ -4,63 +4,73 @@ import com.aridclown.intellij.defold.util.DefoldIcons
 import com.intellij.ide.FileIconProvider
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
-import java.util.*
 import javax.swing.Icon
+
+private val FILE_EXTENSION_TO_ICON = mapOf(
+    // Specific file names
+    "game.project" to "game_project",
+
+    // Scripts
+    "lua" to "script",
+    "script" to "script_type",
+    "editor_script" to "script_type",
+    "gui_script" to "script_type",
+    "render_script" to "script_type",
+
+    // Shaders
+    "fp" to "fragment_shader",
+    "vp" to "vertex_shader",
+    "glsl" to "vertex_shader",
+    "cp" to "vertex_shader",
+    "compute" to "vertex_shader",
+
+    // Assets and resources
+    "animationset" to "animation_set",
+    "appmanifest" to "app_manifest",
+    "atlas" to "atlas",
+    "buffer" to "texture",
+    "camera" to "camera",
+    "collection" to "collection",
+    "collectionfactory" to "collection_factory",
+    "collectionproxy" to "collection_proxy",
+    "collisionobject" to "collision_object",
+    "collisiongroups" to "collision_group",
+    "cubemap" to "cubemap",
+    "display_profiles" to "display_profiles",
+    "displayprofiles" to "display_profiles",
+    "emitter" to "particlefx_emitter",
+    "factory" to "factory",
+    "font" to "font",
+    "gamepads" to "gamepad",
+    "go" to "game_object",
+    "gui" to "gui",
+    "input_binding" to "input_binding",
+    "label" to "gui_text_node",
+    "material" to "material",
+    "mesh" to "mesh",
+    "model" to "model",
+    "particlefx" to "particlefx",
+    "render" to "render",
+    "render_target" to "layers",
+    "sound" to "sound",
+    "sprite" to "sprite",
+    "texture_profiles" to "texture_profile",
+    "textureprofiles" to "texture_profile",
+    "tilemap" to "tilemap",
+    "tilesource" to "tilesource"
+)
 
 class DefoldFileIconProvider : FileIconProvider {
 
-    private val iconByFileName: Map<String, Icon> = mapOf(
-        DefoldConstants.GAME_PROJECT_FILE to DefoldIcons.gameProject
-    )
-
-    private val scriptExtensions = setOf("lua")
-    private val scriptedResourceExtensions = setOf("script", "editor_script", "gui_script", "render_script")
-    private val shaderExtensions = setOf("vp", "glsl", "cp", "compute")
-
     override fun getIcon(file: VirtualFile, flags: Int, project: Project?): Icon? {
-        iconByFileName[file.name]?.let { return it }
-
-        val extension = file.extension?.lowercase(Locale.ROOT) ?: return null
-
-        return when (extension) {
-            in scriptExtensions -> DefoldIcons.script
-            in scriptedResourceExtensions -> DefoldIcons.scriptType
-            in shaderExtensions -> DefoldIcons.vertexShader
-            "fp" -> DefoldIcons.fragmentShader
-            "animationset" -> DefoldIcons.animationSet
-            "appmanifest" -> DefoldIcons.appManifest
-            "atlas" -> DefoldIcons.atlas
-            "buffer" -> DefoldIcons.texture
-            "camera" -> DefoldIcons.camera
-            "collection" -> DefoldIcons.collection
-            "collectionfactory" -> DefoldIcons.collectionFactory
-            "collectionproxy" -> DefoldIcons.collectionProxy
-            "collisionobject" -> DefoldIcons.collisionObject
-            "collisiongroups" -> DefoldIcons.collisionGroup
-            "cubemap" -> DefoldIcons.cubemap
-            "display_profiles" -> DefoldIcons.displayProfiles
-            "emitter" -> DefoldIcons.particlefxEmitter
-            "factory" -> DefoldIcons.factory
-            "font" -> DefoldIcons.font
-            "gamepads" -> DefoldIcons.gamepad
-            "go" -> DefoldIcons.gameObject
-            "gui" -> DefoldIcons.gui
-            "input_binding" -> DefoldIcons.inputBinding
-            "label" -> DefoldIcons.guiTextNode
-            "material" -> DefoldIcons.material
-            "mesh" -> DefoldIcons.mesh
-            "model" -> DefoldIcons.model
-            "particlefx" -> DefoldIcons.particlefx
-            "render" -> DefoldIcons.render
-            "render_target" -> DefoldIcons.layers
-            "sound" -> DefoldIcons.sound
-            "sprite" -> DefoldIcons.sprite
-            "texture_profiles" -> DefoldIcons.textureProfile
-            "tilemap" -> DefoldIcons.tilemap
-            "tilesource" -> DefoldIcons.tilesource
-            "displayprofiles" -> DefoldIcons.displayProfiles
-            "textureprofiles" -> DefoldIcons.textureProfile
-            else -> null
+        // Check for specific file names first
+        FILE_EXTENSION_TO_ICON[file.name]?.let { iconName ->
+            return DefoldIcons.getIconByName(iconName)
         }
+
+        // Then check by extension
+        val extension = file.extension?.lowercase() ?: return null
+        val iconName = FILE_EXTENSION_TO_ICON[extension] ?: return null
+        return DefoldIcons.getIconByName(iconName)
     }
 }
--- src/main/kotlin/com/aridclown/intellij/defold/util/DefoldIcons.kt
+++ src/main/kotlin/com/aridclown/intellij/defold/util/DefoldIcons.kt
@@ -1,48 +1,19 @@
 package com.aridclown.intellij.defold.util
 
-import com.intellij.openapi.util.IconLoader.getIcon
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
 
 object DefoldIcons {
     @JvmField
-    val defoldIcon = getIcon("/icons/icon-ver-outline-white.png", javaClass)
+    val defoldOutlineIcon = icon("/icons/icon-ver-outline-white.png")
 
-    val animationSet = icon("animation_set.svg")
-    val appManifest = icon("app_manifest.svg")
-    val atlas = icon("atlas.svg")
-    val camera = icon("camera.svg")
-    val collection = icon("collection.svg")
-    val collectionFactory = icon("collection_factory.svg")
-    val collectionProxy = icon("collection_proxy.svg")
-    val collisionGroup = icon("collision_group.svg")
-    val collisionObject = icon("collision_object.svg")
-    val cubemap = icon("cubemap.svg")
-    val displayProfiles = icon("display_profiles.svg")
-    val factory = icon("factory.svg")
-    val font = icon("font.svg")
-    val fragmentShader = icon("fragment_shader.svg")
-    val gameObject = icon("game_object.svg")
-    val gameProject = icon("game_project.svg")
-    val gamepad = icon("gamepad.svg")
-    val gui = icon("gui.svg")
-    val guiTextNode = icon("gui_text_node.svg")
-    val inputBinding = icon("input_binding.svg")
-    val layers = icon("layers.svg")
-    val material = icon("material.svg")
-    val mesh = icon("mesh.svg")
-    val model = icon("model.svg")
-    val particlefx = icon("particlefx.svg")
-    val particlefxEmitter = icon("particlefx_emitter.svg")
-    val render = icon("render.svg")
-    val sound = icon("sound.svg")
-    val script = icon("script.svg")
-    val scriptType = icon("script_type.svg")
-    val sprite = icon("sprite.svg")
-    val texture = icon("texture.svg")
-    val textureProfile = icon("texture_profile.svg")
-    val tilemap = icon("tilemap.svg")
-    val tilesource = icon("tilesource.svg")
-    val unknown = icon("unknown.svg")
-    val vertexShader = icon("vertex_shader.svg")
+    private val iconCache = mutableMapOf<String, Icon?>()
 
-    private fun icon(name: String) = getIcon("/icons/defold/$name", javaClass)
+    fun getIconByName(name: String): Icon? = iconCache.getOrPut(name) {
+        defoldIcon("$name.svg")
+    }
+
+    private fun icon(name: String) = IconLoader.getIcon(name, javaClass)
+
+    private fun defoldIcon(name: String) = IconLoader.getIcon("/icons/defold/$name", javaClass)
 }
