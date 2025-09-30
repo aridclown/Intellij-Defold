bc6c66994706c322b3e0236766cc15f172c0da1f
Diogo <diogosouzac@gmail.com>
2025-09-30 19:11:28 -0400

    feat: refactor build actions to simplify class structure and update icons

--- src/main/kotlin/com/aridclown/intellij/defold/actions/DefoldBuildActions.kt
+++ src/main/kotlin/com/aridclown/intellij/defold/actions/DefoldBuildActions.kt
@@ -5,23 +5,17 @@ import com.aridclown.intellij.defold.DefoldProjectRunner
 import com.aridclown.intellij.defold.DefoldProjectService.Companion.createConsole
 import com.aridclown.intellij.defold.DefoldProjectService.Companion.defoldProjectService
 import com.aridclown.intellij.defold.DefoldProjectService.Companion.findActiveConsole
-import com.aridclown.intellij.defold.ui.DefoldLogHyperlinkFilter
 import com.aridclown.intellij.defold.ui.NotificationService.notifyError
 import com.intellij.execution.configuration.EnvironmentVariablesData.DEFAULT
-import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.ActionUpdateThread.BGT
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
 import org.jetbrains.kotlin.utils.addToStdlib.ifFalse
 import org.jetbrains.kotlin.utils.addToStdlib.ifTrue
-import javax.swing.Icon
 
 abstract class AbstractDefoldBuildAction(
-    text: String,
-    description: String,
-    val icon: Icon,
     private val buildCommands: List<String>,
-) : DumbAwareAction(text, description, icon) {
+) : DumbAwareAction() {
 
     override fun getActionUpdateThread() = BGT
 
@@ -56,15 +50,9 @@ abstract class AbstractDefoldBuildAction(
 }
 
 class DefoldBuildProjectAction : AbstractDefoldBuildAction(
-    text = "Build Project",
-    description = "Build the Defold project",
-    icon = AllIcons.Actions.Compile,
     buildCommands = listOf("build"),
 )
 
-class DefoldRebuildProjectAction : AbstractDefoldBuildAction(
-    text = "Rebuild Project",
-    description = "Clean and build the Defold project",
-    icon = AllIcons.Actions.Rebuild,
+class DefoldCleanBuildProjectAction : AbstractDefoldBuildAction(
     buildCommands = listOf("distclean", "build"),
 )
