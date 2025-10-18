0713549c3135ab53bb1bcbd665c8ef41c083b20e
Diogo <diogosouzac@gmail.com>
2025-10-18 14:03:42 +0900

    test: add unit tests for local variable and vararg parameter navigation

---
+++ src/test/kotlin/com/aridclown/intellij/defold/debugger/value/navigation/SourceNavigationTest.kt
@@ -0,0 +1,62 @@
+package com.aridclown.intellij.defold.debugger.value.navigation
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import com.intellij.xdebugger.XSourcePosition
+import com.intellij.xdebugger.frame.XNavigatable
+import com.intellij.xdebugger.impl.XSourcePositionImpl
+import org.assertj.core.api.Assertions.assertThat
+
+private class CapturingNavigatable : XNavigatable {
+    var position: XSourcePosition? = null
+
+    override fun setSourcePosition(sourcePosition: XSourcePosition?) {
+        position = sourcePosition
+    }
+}
+
+class SourceNavigationTest : BasePlatformTestCase() {
+
+    fun `test navigates to local variable declaration`() {
+        val luaFile = myFixture.configureByText(
+            "script.lua",
+            """
+            function foo()
+                local testVar = 1
+                print(testVar)
+            end
+            """.trimIndent()
+        )
+        myFixture.openFileInEditor(luaFile.virtualFile)
+
+        val sourcePosition = XSourcePositionImpl.create(luaFile.virtualFile, 2)!!
+        val navigatable = CapturingNavigatable()
+
+        navigateToLocalDeclaration(project, sourcePosition, "testVar", navigatable)
+
+        val declarationPosition = navigatable.position ?: error("Declaration not found")
+        val declarationLine = myFixture.editor.document.getLineNumber(declarationPosition.offset)
+        assertThat(declarationLine).isEqualTo(1)
+    }
+
+    fun `test navigates to vararg parameter`() {
+        val luaFile = myFixture.configureByText(
+            "script.lua",
+            """
+            function foo(...)
+                local first = ...
+                print(first)
+            end
+            """.trimIndent()
+        )
+        myFixture.openFileInEditor(luaFile.virtualFile)
+
+        val sourcePosition = XSourcePositionImpl.create(luaFile.virtualFile, 2)!!
+        val navigatable = CapturingNavigatable()
+
+        navigateToLocalDeclaration(project, sourcePosition, "...", navigatable)
+
+        val declarationPosition = navigatable.position ?: error("Vararg declaration not found")
+        val declarationLine = myFixture.editor.document.getLineNumber(declarationPosition.offset)
+        assertThat(declarationLine).isEqualTo(0)
+}
+}
