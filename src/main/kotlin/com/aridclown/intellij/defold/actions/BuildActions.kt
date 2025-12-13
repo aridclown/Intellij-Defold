1e84498b9f0add6aaa1b9b3af3e4d384b1cb5fa9
Arid Clown <764655+aridclown@users.noreply.github.com>
2025-12-13 18:16:07 -0500

    Refactor build action handling

--- src/main/kotlin/com/aridclown/intellij/defold/actions/BuildActions.kt
+++ src/main/kotlin/com/aridclown/intellij/defold/actions/BuildActions.kt
@@ -5,13 +5,16 @@ import com.aridclown.intellij.defold.debugger.MobDebugRunConfiguration
 import com.intellij.execution.ProgramRunnerUtil
 import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 
 abstract class AbstractBuildAction(
     private val buildCommands: List<String>
 ) : DefoldProjectAction() {
 
-    override fun actionPerformed(event: AnActionEvent) = withDefoldProject(event) { project ->
+    final override fun actionPerformed(event: AnActionEvent) = withDefoldProject(event, ::performBuild)
+
+    protected open fun performBuild(project: Project) {
         withDefoldConfig(project) {
             val settings = DefoldRunConfigurationUtil.getOrCreate(project)
             val runConfiguration = settings.configuration as? MobDebugRunConfiguration ?: return@withDefoldConfig
@@ -27,7 +30,7 @@ abstract class AbstractBuildAction(
 class BuildProjectAction : AbstractBuildAction(buildCommands = listOf("build"))
 
 class CleanBuildProjectAction : AbstractBuildAction(buildCommands = listOf("distclean", "build")) {
-    override fun actionPerformed(event: AnActionEvent) = withDefoldProject(event) { project ->
+    override fun performBuild(project: Project) {
         val confirmed = Messages.showOkCancelDialog(
             project,
             "Are you sure you want to perform a clean build?",
@@ -37,8 +40,8 @@ class CleanBuildProjectAction : AbstractBuildAction(buildCommands = listOf("dist
             Messages.getQuestionIcon()
         ) == Messages.OK
 
-        if (!confirmed) return@withDefoldProject
+        if (!confirmed) return
 
-        super.actionPerformed(event)
+        super.performBuild(project)
     }
 }
